7811273c1adb9dd8ecb290d45a7b01d95849ff1f
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-08-29 23:27:26 +0530

    rebuilt sfc as a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Person from "./components/Person";
+import Switch from "@material-ui/core/Switch";
+import Container from "@material-ui/core/Container";
+import Nav from "./components/AppBar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    people: [
+      { id: 1, name: "kalana", age: 23 },
+      { id: 2, name: "pinkPanther", age: 20 },
+      { id: 3, name: "dumbo", age: 21 },
+    ],
+    toggleShow: false,
+    result: "",
+  };
+
+  nameChangeHandler = (event, id) => {
+    const personId = this.state.people.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.people[personId],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.people];
+
+    persons[personId] = person;
+
+    this.setState({
+      people: persons,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Container maxWidth="sm">
+          <Switch
+            checked={this.state.toggleShow}
+            onChange={() =>
+              this.setState({
+                ...this.state,
+                toggleShow: !this.state.toggleShow,
+              })
+            }
+            name="checkedA"
+            inputProps={{ "aria-label": "secondary checkbox" }}
+          />
+
+          {this.state.toggleShow ? (
+            this.state.people.map((person, index) => {
+              return (
+                <Person
+                  key={person.id}
+                  index={person.id}
+                  namechange={(event) =>
+                    this.nameChangeHandler(event, person.id)
+                  }
+                  name={person.name}
+                />
+              );
+            })
+          ) : (
+            <h1>Hidden</h1>
+          )}
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Person/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+const Index = (props) => {
+  return (
+    <div>
+      <Card
+        bgcolor="warning.main"
+        variant="outlined"
+        // style={{ margin: "0 100px" }}
+      >
+        <CardContent>
+          <br />
+          {props.name ? (
+            <Button variant="contained" color="secondary">
+              {props.name}
+            </Button>
+          ) : null}
+          <br />
+          <br />
+          <Button variant="contained" color="primary">
+            {props.index}
+          </Button>
+          <br />
+          <br />
+          <br /> <br />
+          <TextField
+            id={props.name}
+            onChange={props.namechange}
+            label="Enter your name here"
+            variant="outlined"
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Index;
